2c03254c7ffc114f5ebcd7dd68f13e99e5e5be26
Adeniran Moses <thewebstr25@gmail.com>
2025-08-27 19:06:27 +0100

    Add mobile navigation functionality with hamburger menu and close button

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById("ham").addEventListener("click", function(){
+
+  document.getElementById("nav").style.transform = "translateX(0)";
+
+  document.getElementById("ham").style.display = "none";
+
+  document.getElementById("close").style.display = "flex";
+
+});
+document.getElementById("close").addEventListener("click", function(){
+
+  document.getElementById("nav").style.transform = "translateX(100%)";
+
+  document.getElementById("ham").style.display = "flex";
+
+  document.getElementById("close").style.display = "none";
+
+});
